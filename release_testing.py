9db98475f5fc896652cbfa60df32c3a7613fd167
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-04-02 11:37:12 +0100

    Create release_testing.py

---
+++ release_testing.py
@@ -0,0 +1,33 @@
+import re
+import subprocess
+
+# Run pip list and filter for "ovos-" packages
+result = subprocess.run(['pip', 'list', '--format=freeze'], capture_output=True, text=True)
+packages = result.stdout.splitlines()
+
+
+testing_constraints = [
+    "onnxruntime<=1.20.1"  # 1.21.0 breaks raspberry pi
+]
+
+# Regular expression to capture package name and version, with optional alpha tag
+pattern = re.compile(r'^(ovos-[\w-]+)==(\d+)\.(\d+)\.(\d+)(a\d+)?$')
+
+for package in packages:
+    match = pattern.match(package)
+    if match:
+        name, major, minor, patch, alpha = match.groups()
+        minver = f"{major}.{minor}.{patch}"
+        if alpha:
+            minver += f"{alpha}"
+        major = stable_major = int(major)
+        minor = stable_minor = int(minor)
+        patch = int(patch)
+        print(package)
+        testing_constraints.append(f"{name}>={minver},<{major+1}.0.0")
+
+
+with open('constraints-testing.txt', 'w') as testing_file:
+    testing_file.write("\n".join(testing_constraints))
+    
+print("Constraints files generated: constraints-testing.txt")
