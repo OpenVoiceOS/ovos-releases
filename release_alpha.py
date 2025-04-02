cbbd3666bd5fa5a8594658fdefe999f077c17ede
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-04-02 11:30:42 +0100

    Update release_alpha.py

--- release_alpha.py
+++ release_alpha.py
@@ -5,7 +5,9 @@ import subprocess
 result = subprocess.run(['pip', 'list', '--format=freeze'], capture_output=True, text=True)
 packages = result.stdout.splitlines()
 
-alpha_constraints = []
+alpha_constraints = [
+    "onnxruntime==1.20.1"  # 1.21.0 breaks raspberry pi
+]
 
 # Regular expression to capture package name and version, with optional alpha tag
 pattern = re.compile(r'^(ovos-[\w-]+)==(\d+)\.(\d+)\.(\d+)(a\d+)?$')
